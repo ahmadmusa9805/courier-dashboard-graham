b3a83ecc8264abd214724fc26292a18914148680
Ahmad Musa Abdullah <ahmadmusa9805@gmail.com>
2026-03-25 22:32:50 +0600

    update item module

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -6,7 +6,7 @@ import {
 } from "./requestModifications";
 import { API_BASE_URL } from "../constants";
 
-const BASE_URL = API_BASE_URL || "http://api.koerierplatform.nl/api/v1" || "http://13.61.230.38:5001//api/v1";
+const BASE_URL = API_BASE_URL || "https://api.koerierplatform.nl/api/v1" 
 
 const httpRequest = (
   config = {
